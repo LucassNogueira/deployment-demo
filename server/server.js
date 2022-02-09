377dfbe05b30c96a256999a12f32c21b6d78f35a
LucassNogueira <simplylucasjason@gmail.com>
2022-02-09 11:58:37 -0600

    getting heroku set up

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-// app.get();
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
-app.listen(4000, () => console.log("listening on port 4000"));
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => console.log("listening on " + port));
